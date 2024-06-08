663ab4ef8903c5aebf326b9448fc1b539a3d1585
Amit Roy <119528393+amitroy4@users.noreply.github.com>
2024-06-08 23:58:06 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p className="text-green-600">
+        Initail Setup for Social medial by Amit Roy
+      </p>
+    </>
+  );
+}
+
+export default App;
