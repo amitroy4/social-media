7995c83eabff1829a747f3722da5a103f4ba3613
Amit Roy <119528393+amitroy4@users.noreply.github.com>
2024-07-11 23:06:33 +0600

    Redux tool kit setup done

---
+++ src/features/users/authSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  userInfo: null,
+};
+
+export const userSlice = createSlice({
+  name: "authUser",
+  initialState,
+  reducer: {
+    createUser: (state, action) => {
+      state.userInfo = action.payload;
+    },
+  },
+});
+
+export const { createUser } = userSlice.actions;
+
+export default userSlice.reducer;
