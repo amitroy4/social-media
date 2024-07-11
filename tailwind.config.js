f0e6510ab72c71ab3b2e546e1aa88e156a6761f6
Amit Roy <119528393+amitroy4@users.noreply.github.com>
2024-07-11 23:27:30 +0600

    CSS variable setup done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,14 @@
 export default {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
+    colors: {
+      main_bg: "var(--main_bg)",
+      page_color: "var(--page_color)",
+      input_color: "var(--input_color)",
+      line_color: "var(--line_color)",
+      primary_bg: "var(--primary_bg)",
+    },
     extend: {},
   },
   plugins: [],
 };
-
